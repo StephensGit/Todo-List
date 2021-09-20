8cf2a671251c42efd318a71aa4089e53aeeb97db
StephensGit <stephenmoran@live.ie>
2021-09-20 17:27:32 +0100

    Filter method added for 'all', 'active', & 'completed'

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-// const theme_toggler = document.querySelector('#theme_toggler');
 // reference to the form
 const form = document.querySelector('.form');
 // reference to the ul, to later add new todos
 const list = document.querySelector('ul');
+// reference clear button
+const clear = document.querySelector('.clear');
+
+// Array to store all todos
+let todos = [];
 
 // function to create todo
 const generateTodoTemplate = (todo) => {
+  todos.push(todo);
+  console.log(todos);
   const html = `
 
     <li class="flex-row align-content-center list-item">
@@ -21,28 +27,73 @@ const generateTodoTemplate = (todo) => {
   list.innerHTML += html;
 };
 
+// Event Listener for submitting form
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+  //   Trim any white spaces (create - is the data-attribute for name)
   const todo = form.create.value.trim();
 
   //   Stops users adding empty todos
   if (todo.length) {
+    //   Invoke function to create new todo
     generateTodoTemplate(todo);
     form.reset();
   }
 });
 
-// delete todo item
+// Delete todo item
+const removeTodoItem = (item) => {
+  item.remove();
+};
+
 list.addEventListener('click', (e) => {
   if (e.target.classList.contains('cross-icon')) {
-    e.target.parentElement.remove();
+    removeTodoItem(e.target.parentElement);
   }
-  // complete todo item
+  // Complete todo item
   else if (e.target.classList.contains('checkmark')) {
     e.target.nextElementSibling.classList.add('complete-todo');
   }
 });
 
-// theme_toggler.addEventListener('click', () => {
-//   document.body.classList.toggle('dark_mode');
-// });
+// Clear all completed items
+clear.addEventListener('click', () => {
+  document
+    .querySelectorAll('.list-item input[type="checkbox"]:checked')
+    .forEach((item) => {
+      removeTodoItem(item.closest('li'));
+    });
+});
+
+// filter todo list items => for every element pass the
+document.querySelectorAll('.item-state').forEach((elem) => {
+  elem.addEventListener('click', (e) => {
+    filterTodoItems(e.target.id);
+  });
+});
+
+const filterTodoItems = (id) => {
+  const allItems = list.querySelectorAll('li');
+
+  switch (id) {
+    case 'all':
+      allItems.forEach((item) => {
+        item.classList.remove('hidden');
+      });
+      break;
+    case 'active':
+      allItems.forEach((item) => {
+        item.querySelector('input').checked
+          ? item.classList.add('hidden')
+          : item.classList.remove('hidden');
+      });
+      break;
+    default:
+      allItems.forEach((item) => {
+        !item.querySelector('input').checked
+          ? item.classList.add('hidden')
+          : item.classList.remove('hidden');
+      });
+      break;
+  }
+};
